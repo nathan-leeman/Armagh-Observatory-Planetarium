c54f589c1f88ec8baf0c52d33381da29723e9e64
Nathan Leeman <nathan12167@gmail.com>
2019-04-11 12:07:34 +0100

    Created a new page for the Human Orrery based on old files from the old site

---
+++ humanorrery.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+//declare password
+
+include("functions.php");
+include("connection.php")
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AOP</title>
+
+    <!-- Bootstrap core CSS -->
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/small-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+
+</head>
+
+<body>
+
+
+<?php
+navbar();
+
+echo "  <div class='row'>
+              
+            
+                  <div class='col-12'>
+                 
+                      
+                     <div class='jumbotron jumbotron-fluid' style='margin-top: 10px;'>
+  <div class='container'>
+    <h1 class='display-4 text-center'>Human Orrery</h1>
+    
+    <p class='lead'>The Human Orrery is the latest addition to the grounds of Armagh Observatory and provides a unique investigation of planetary motion. It is also fun to use with the capacity to present fundamental ideas in astronomy, mathematics and space science to as wide an audience as possible.</p>
+<hr class='my-4'>
+<p class='lead'>A Brief Historical Background</p>
+<hr class='my-4'>
+<p>The first orrery was conceived by English clockmaker and inventor George Graham (c.1674-1751) around 300 years ago. This initial model only showed the earth-moon system which orbits our Sun. Graham gave the design of this original model to the celebrated London instrument maker John Rowley, who was commissioned to make one for his patron Charles Boyle (1674-1731). Boyle's patronage of Rowley soon led to the elaboration of Graham's invention so that it included all the known planets and some moons of the solar system. The origin of the term \"orrery\" is explained when we consider the title Boyle held - the fourth Earl of Orrery (Orrery being the old name for a part of Co. Cork).</p>
+    <hr class='my-4'>
+    
+    
+    <p class='lead'>
+    Structure of the Human Orrery
+    </p>
+    <hr class='my-4'>
+    <p>Armagh Observatory's Human Orrery is interactive: it allows people to play the part of the moving planets. It features an accurate scale model of the positions and orbits of the Earth and the five other planets in the Solar System known since ancient times (Mercury, Venus, Mars, Jupiter and Saturn), as well as the asteroid Ceres and two comets: 1P/Halley and 2P/Encke. The orbits of these objects are arranged on the ground with stainless steel tiles. Jumping from one tile to the next represents a 16 day time interval for all the planets, except Jupiter and Saturn, whose tile jumps represent a 160 day interval. The tiles for Ceres and the comets have 80 day intervals. More distant objects which could not be accommodated within the dimensions of the Human Orrery are listed on the Outer Ring of the exhibit.
+
+</p>
+
+<div class='row'>
+                  <div class='col-6 text-center'>
+                  <img src='img/orerry/merc.jpg' style='width: 370px; height: 335px;'>
+                  
+                  </div>
+                  <div class='col-6 text-center'>
+                  <img src='img/orerry/solartile1.jpg'>
+                   </div>
+                  
+                  </div>
+                  <hr class='my-4'>
+<a href='https://armaghop.sharepoint.com/:f:/s/HistoryandHeritage/EnHDzEm5Tg1CgMrz5QuhqwABL8tMNUnTr2rFfMYql81Cgw?e=ObWZLg'>
+<h3 class=' text-center'>Check out our Historical Photographic Collection of the Human Orrery</h3></a><hr class='my-4'>
+  </div>
+  
+  
+  
+                      
+                      
+                  </div>
+  
+  
+</div>
+                      
+                      </div>
+                      </div>
+                      
+                  
+                  
+                  
+                  
+                  
+                  
+             
+          
+      ";
+
+
+?>
+
+</div>
+<?php
+footer();
+
+?>
+
+
+
+
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
