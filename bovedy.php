49a59686109e308b6e11a84a301be749c6885a52
Nathan Leeman <nathan12167@gmail.com>
2019-04-11 12:06:57 +0100

    Fixed title

--- bovedy.php
+++ bovedy.php
@@ -40,13 +40,49 @@ echo "  <div class='row'>
                       
                      <div class='jumbotron jumbotron-fluid' style='margin-top: 10px;'>
   <div class='container'>
-    <h1 class='display-4 text-center'>Title</h1>
+    <h1 class='display-4 text-center'> Bovedy Meteorites Anniversary </h1>
     <p class='lead'></p>
   </div>
 </div>
                       
                       </div>
                       </div>
+                      
+                      
+                      <div class='jumbotron'>
+ 
+  <p class='lead'>Northern Ireland, a country filled with history and heritage. A country with rolling hills, mountains, valleys and lakes and also fire balls in the sky. On 25th April 1969, mere months before Apollo 11 was due to land on the Moon, a meteorite fell in our beloved country. This particular meteorite was seen across the British Isles and it broke in two over Northern Ireland. Part of it fell into a Police Store Station in Sprucefield, Lisburn, and part fell into a farmer’s field in the townland of Bovedy in Co. Derry-Londonderry. The remainder of the meteorite continued on and most likely ended up in the ocean beyond. 
+</p>
+<hr class='my-4'>
+<p class='lead'>The Bovedy and Sprucefield meteorites are special not just because they landed in Northern Ireland, but also because they are examples of L3 chondrites. These are often referred to as unequilibrated chondrites because minerals such as olivine and pyroxene show a wide range of compositions, reflecting formation under a wide variety of conditions in the solar nebula. In a nutshell they are great examples for displaying the building blocks of the solar system. 
+</p>
+<hr class='my-4'>
+<p class='lead'>The largest piece of the Bovedy Meteorite is on display in the Planetarium and is a big attraction for people from far a wide. Many people remember when this meteorite fell and one man, artist Noel Connor, was inspired to create artistic works to help celebrate the 50th Anniversary. 
+</p>
+<hr class='my-4'>
+<p class='lead'>The Bovedy and Sprucefield meteorites are special not just because they landed in Northern Ireland, but also because they are examples of L3 chondrites. These are often referred to as unequilibrated chondrites because minerals such as olivine and pyroxene show a wide range of compositions, reflecting formation under a wide variety of conditions in the solar nebula. In a nutshell they are great examples for displaying the building blocks of the solar system. 
+</p>
+<hr class='my-4'>
+<p class='lead'>This Easter we will be celebrating this anniversary by hosting a range of events. The 25th April will see Senior Curator of Natural Science of the Ulster Museum, Dr. Mike Simms, hosting talks for people of all ages. In the evening time, An Evening with Bovedy will see astronomers and other professionals talking about the creation of the elements, the origins of the solar system and everything you need to know about meteorites.
+</p>
+<hr class='my-4'>
+<a href='https://armaghobservatoryplanetarium.ticketsolve.com/shows/1173592621/events/428217296'>
+<h3 class=' text-center'>Book tickets and find out more information</h3></a>
+  <hr class='my-4'>
+  
+</div>
+                      
+                      
+                      
+                     
+
+
+
+
+
+                      
+                      
+                      
                   </div>
                   
                   
