2411333a6eebde8a19d39ea9edbcc8d5a168afb4
Nathan Leeman <nathan12167@gmail.com>
2019-05-20 12:33:58 +0100

    Setup for temporary usage for BoG. Pages not needed

---
+++ bog2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+//declare password
+
+include("functions.php");
+include("connection.php")
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AOP</title>
+
+    <!-- Bootstrap core CSS -->
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/small-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+
+</head>
+
+<body>
+
+
+<?php
+navbar();
+
+echo "
+<div class='jumbotron' style='margin-top: 10px;'>
+
+  <div class='container'>
+    <h1 class='display-4'>Board of Governors 
+</h1>
+<hr class='my-4'>
+
+ 
+
+
+  
+
+</div>
+</div>
+
+
+<div class='jumbotron' style='margin-top: 10px;'>
+
+  <div class='container'>
+  <a href='https://armaghop.sharepoint.com/sites/NLeeman/Shared%20Documents/Forms/AllItems.aspx?viewid=06e6ebca-dc88-40ec-9070-5ce33aec48b5&id=%2Fsites%2FNLeeman%2FShared%20Documents%2FBoard%20of%20Governors'>
+  <h1 class='display-4'>BoG Documentation
+</h1>
+    
+
+
+ </a>
+
+
+  
+
+</div>
+</div>
+
+
+
+
+
+";
+
+?>
+
+</div>
+<?php
+footer();
+
+?>
+
+
+
+
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
