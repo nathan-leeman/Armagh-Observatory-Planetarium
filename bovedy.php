0feb7867e3d1e1005f097b6bb71e74c733be686d
Nathan Leeman <nathan12167@gmail.com>
2019-05-20 13:54:22 +0100

    Attempted adding photos with a carousel but they aren't formatted correctly for this

--- bovedy.php
+++ bovedy.php
@@ -44,9 +44,44 @@ echo "  <div class='row'>
     <p class='lead'></p>
   </div>
 </div>
+
+
+
+
                       
                       </div>
                       </div>
+                      
+                      <div class='row'>
+                      
+                      <div class='col-12'>
+                      
+                      <div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
+  <div class='carousel-inner'>
+    <div class='carousel-item active' style='max-height: 650px;'>
+      <img src='img/bovedy/Bovedy%20Postcard%20-%20no%20text.jpg' class='d-block w-100' alt='...'>
+    </div>
+    <div class='carousel-item'  style='max-height: 650px;'>
+      <img src='img/bovedy/5x4%20approx%20300.%20H%20JPG.JPG' class='d-block w-100' alt='...'>
+    </div>
+    <div class='carousel-item'  style='max-height: 650px;'>
+      <img src='img/bovedy/for%20website.JPG' class='d-block w-100' alt='...'>
+    </div>
+  </div>
+  <a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
+    <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+    <span class='sr-only'>Previous</span>
+  </a>
+  <a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
+    <span class='carousel-control-next-icon' aria-hidden='true'></span>
+    <span class='sr-only'>Next</span>
+  </a>
+</div>
+                      
+                      
+</div>
+                      
+</div>
                       
                       
                       <div class='jumbotron'>
