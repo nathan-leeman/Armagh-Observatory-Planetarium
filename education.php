716219c2d228fba45c04b9afd8b1097b55524151
Nathan Leeman <nathan12167@gmail.com>
2019-05-20 12:34:36 +0100

    Copied for usage with BoG page. BoG pages not used

--- education.php
+++ education.php
@@ -85,6 +85,55 @@ echo "
 </div>
 
 
+<div class='jumbotron' style='margin-top: 10px;'>
+
+  <div class='container'>
+  <a href='' data-toggle='modal' data-target='#exampleModalCenter'>
+  <h1 class='display-4'>Crescent Moon Competition
+</h1>
+    
+
+
+ </a>
+<!-- Modal -->
+<div class='modal fade' id='exampleModalCenter' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+  <div class='modal-dialog modal-dialog-centered' role='document'>
+    <div class='modal-content'>
+      <div class='modal-header'>
+        <h5 class='modal-title' id='exampleModalCenterTitle'>Crescent Moon Competition</h5>
+        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+      <div class='modal-body'>
+      <p class='lead text-center'>Please download the poster below and read the information</p>
+      <a href='education/AOP-school-competition-poster.pdf'>
+      <p class='lead text-center'>Download Poster </p></a>
+      <a href='education/AOP-moon-competition-templates.pdf'>
+        <p class='lead text-center'>Download Templates </p>  </a>
+     
+        
+      </div>
+      <div class='modal-footer text-center'>
+        
+         <a href='education/AOP-school-competition-poster.pdf'>
+        <img src='education/AOP-school-competition-poster.jpg' style='width: 470px;'></a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+  <a href='education/AOP-moon-competition-templates.pdf'></a>
+
+</div>
+</div>
 
 
 ";
